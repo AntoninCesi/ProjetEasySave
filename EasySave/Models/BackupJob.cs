68d899d8e57db5ea9db7d7a564af6508460e9fdb
LuckasSalome <luckas.salome@viacesi.fr>
2026-01-30 11:13:46 +0100

    Feat: implement BackupJob model and BackupType enum

---
+++ EasySave/Models/BackupJob.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasySave.Models;
+public enum BackupType
+{
+    Full,
+    Differential
+}
+
+public class BackupJob
+{
+    public string Name { get; set; } = string.Empty;
+    public string SourcePath { get; set; } = string.Empty;
+    public string TargetPath { get; set; } = string.Empty;
+    public BackupType Type { get; set; }
+}
