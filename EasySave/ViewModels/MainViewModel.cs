57d612f49c17a961a243e16fc8ea29c1ae2b082a
LuckasSalome <luckas.salome@viacesi.fr>
2026-01-30 11:38:55 +0100

    Work: initialize MainViewModel with services integration

---
+++ EasySave/ViewModels/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using EasySave.Models;
+using EasySave.Services;
+using System.Diagnostics;
+
+namespace EasySave.ViewModels;
+
+public class MainViewModel
+{
+    private readonly List<BackupJob> _jobs = new();
+    private readonly LogService _logService = new();
+    private readonly StateService _stateService = new();
+}
