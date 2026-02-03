adb6c49754b9544f7c03b3901d984ca988c64609
Adem <151775074+M0r0cc0@users.noreply.github.com>
2026-02-03 15:27:21 +0100

    Création de BackupMedium

---
+++ EasySave/Models/BackupMedium.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace EasySave
+{
+    public enum StorageType { Local, Externe, Reseau }
+
+    public class BackupMedium
+    {
+        public StorageType storageType { get; set; }
+        private string _drivePath;
+
+        public BackupMedium(string path, StorageType type)
+        {
+            _drivePath = Path.GetPathRoot(path);
+            storageType = type;
+        }
+
+        public long availableSpace()
+        {
+            DriveInfo drive = new DriveInfo(_drivePath);
+            return drive.IsReady ? drive.AvailableFreeSpace : 0;
+        }
+
+        public bool isConnected()
+        {
+            DriveInfo drive = new DriveInfo(_drivePath);
+            return drive.IsReady;
+        }
+    }
+}
