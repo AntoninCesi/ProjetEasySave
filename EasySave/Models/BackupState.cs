73c5133c5b1a14e6da010983d7f686c4299af3cb
Yudracknight <jennifermv0309@gmail.com>
2026-02-11 15:55:20 +0100

    feat(Controller): ajout de l'option pour commencer le backupjob en ligne de commande et via la console

--- EasySave/Models/BackupState.cs
+++ EasySave/Models/BackupState.cs
@@ -3,31 +3,7 @@ using System;
 using Tool.Utils;
 
 
-public class BackupState
-{
-	public int Id { get; set; }
-	public string JobName { get; set; } = string.Empty;
-	public DateTime LastActionTimestamp { get; set; }
 
-	public BackupStatus Status { get; set; }
-
-	public int TotalFiles { get; set; }
-	public long TotalSizeBytes { get; set; }
-
-	public int RemainingFiles { get; set; }
-	public long RemainingSizeBytes { get; set; }
-
-	public float Progress { get; set; }
-
-	public string CurrentSourceUNC { get; set; } = string.Empty;
-	public string CurrentDestinationUNC { get; set; } = string.Empty;
-}
-
-<<<<<<< HEAD
-﻿using System;
-using Tool.Utils;
-=======
->>>>>>> 6fc228043ef822a12c73813042331bf8289c9e3f
 
 namespace EasySave.Models
 {
--- EasySave/Program.cs
+++ EasySave/Program.cs
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 using System;
 using System.Collections.Generic;
-=======
->>>>>>> 6fc228043ef822a12c73813042331bf8289c9e3f
 using EasySave.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -17,10 +14,6 @@ class Program
         Controller controller = new Controller(args);
 
     }
-<<<<<<< HEAD
         
 }
-=======
 
-}
->>>>>>> 6fc228043ef822a12c73813042331bf8289c9e3f
