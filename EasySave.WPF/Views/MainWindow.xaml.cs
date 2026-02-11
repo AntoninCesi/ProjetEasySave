0207b15e9015ecb948ea598d250f012046a09950
LuckasSalome <luckas.salome@viacesi.fr>
2026-02-11 11:53:10 +0100

    feat: implement WPF graphical user interface

---
+++ EasySave.WPF/Views/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using EasySave.WPF.ViewModels;
+
+namespace EasySave.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Assign the ViewModel as the data context
+            DataContext = new MainViewModel();
+        }
+    }
+}
